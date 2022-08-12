0e8b8c1c465c2f603fce8a858dcfaa27f5e90f64
Noa <noaisraeli3@gmail.com>
2022-08-12 16:29:42 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FootballDataService } from './football-data.service';
+import { LeagueTable } from './models/leagueTable';
+import { GameWeek } from './models/gameWeek';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FootballDataService]
 })
-export class AppComponent {
-  title = 'angular-practice';
+export class AppComponent implements OnInit {
+  title = 'app';
+  table: LeagueTable | undefined;
+  gameweek!: GameWeek;
+
+  constructor(private footballData: FootballDataService) {}
+
+  ngOnInit() {
+    this.getTable();
+  }
+
+  getTable() {
+    this.footballData.getTable()
+      .subscribe(
+        data => {
+          this.table = data;
+          this.getFixtures(data.matchday);
+        },
+        error => console.log(error)
+      );
+  }
+
+  getFixtures(matchDay: number) {
+    this.footballData.getFixtures(matchDay)
+      .subscribe(
+        data => this.gameweek = data,
+        error => console.log(error)
+      );
+  }
+
 }
